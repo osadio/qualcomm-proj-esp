bdd7566be10789c0f496935fb6f7203b935441ff
osadio <osadio@outlook.com>
2023-01-02 03:47:37 +0000

    Create mn_startup_server.sh

---
+++ formal-version/mn_startup_server.sh
@@ -0,0 +1,16 @@
+uam_h0 python ./servers/uam_h0.py &
+
+n_URLLC_h0 python ./servers/n_URLLC_h0.py &
+n_eMBB_h0 python ./servers/n_eMBB_h0.py &
+n_mMTC_h0 python ./servers/n_mMTC_h0.py &
+
+s_NRF_h0 python ./servers/s_NRF_h0.py &
+s_AUSF_h0 python ./servers/s_AUSF_h0.py &
+s_UDM_h0 python ./servers/s_UDM_h0.py &
+s_AMF_h0 python ./servers/s_AMF_h0.py &
+s_SMF_h0 python ./servers/s_SMF_h0.py &
+s_PCF_h0 python ./servers/s_PCF_h0.py &
+s_AF_h0 python ./servers/s_AF_h0.py &
+
+d_UPF_h0 python ./servers/d_UPF_h0.py &
+d_DN python ./servers/d_DN.py &
